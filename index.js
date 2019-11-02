736b5270bc6bdab6076491cc7a5089672bc2ef76
Alan Campbell <alan.campbell@imatrixsys.com>
2019-11-02 10:43:07 -0700

    Reorganized and refactor of expressAPI, added parsing middleware, fixed request handling

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-const express = require('express')
-const path 	  = require('path')
-const axios   = require('axios')
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-const { IamAuthenticator } = require('ibm-watson/auth')
+const express         = require('express')
+const path            = require('path')
+const axios           = require('axios')
+const bodyParser      = require('body-parser')
 
 const app = express()
 
-const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({
-    apikey:'s8NneGprypcjshsq9xFNEECgB-NT_0F_4l-R34bN1qHR',
-  }),
-  url: 'https://gateway.watsonplatform.net/tone-analyzer/api'
-})
+app
+  .use(express.static(path.join(__dirname, './client/public'))) // Serve the static files from the React app
+  .use(bodyParser.urlencoded({ extended: true }))               // Allows req.body
+  .use(bodyParser.json())                                       // Parsing Middleware
+  .use('/api', require('./expressAPI/index'))                   // Used for external APIs unrelated to AWS Serverless Architecture
+  .get('*', (req,res) => res.sendFile(path.join(__dirname+'./client/public/index.html')))  // Handles unknown requests
 
+const port = process.env.PORT || 5000
+app.listen(port)
 
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/public')))
-
-// An api endpoint that accesses Watson's Tone Analyzer
-app.get('/api/analyzeEntry/:content', (req,res, next) => {
-  const { content } = req.params
-  const toneParams = {toneInput: { 'text': content }, contentType: 'application/json'}
-  toneAnalyzer.tone(toneParams)
-  .then(toneAnalysis => res.send(toneAnalysis))
-  .catch(err => console.log('error:', err))
-})
-
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('App is listening on port ' + port);
+console.log('App is listening on port ' + port)
