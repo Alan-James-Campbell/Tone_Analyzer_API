2aa31b104bdabeb184bc8d98bfbc71e10ac805f4
Alan-James-Campbell <alan.campbell@imatrixsys.com>
2019-11-19 22:54:21 -0800

    ch

--- tone-analyzer-api/analyzeEntry.js
+++ tone-analyzer-api/analyzeEntry.js
@@ -1,13 +1,14 @@
 import { failure } from "./libs/response-lib";
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
-const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({apikey:'s8NneGprypcjshsq9xFNEECgB-NT_0F_4l-R34bN1qHR'}),
-  url: 'https://gateway.watsonplatform.net/tone-analyzer/api'
-});
+
 
 export async function main(event, context) {
+  const toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21',
+    authenticator: new IamAuthenticator({apikey:'s8NneGprypcjshsq9xFNEECgB-NT_0F_4l-R34bN1qHR'}),
+    url: 'https://gateway.watsonplatform.net/tone-analyzer/api'
+  });
   const data = JSON.parse(event.body);
   const text = data.text;
   const toneParams = {toneInput: { text }, contentType: 'application/json'};
